90ff3f5fbff961a65a59520adc5280e7bf231bc4
Isil Candemir <32885773+isilcandemir@users.noreply.github.com>
2018-02-24 21:56:49 +0300

    Add files via upload

---
+++ GoldenCross.R
@@ -0,0 +1,48 @@
+library(zoo)
+library(Quandl)
+library(quantmod)
+getSymbols("SPY",src="yahoo")
+P<-SPY
+tail(P)
+library(FinancialInstrument)
+currency("USD")
+stock("SPY",currency="USD",multiplier=1)
+ls(envir=FinancialInstrument:::.instrument)
+library(xts)
+Sys.setenv(TZ="UTC")
+SPY<-xts(coredata(SPY),as.POSIXct(time(SPY)))
+SPY$MA50<-SMA(Cl(SPY),50)
+SPY$MA200<-SMA(Cl(SPY),200)
+my.strategy<-"GoldenCross"
+updateAcct(my.strategy,portfolios=my.strategy, initDate = "2006-12-31",initEq = 1000000)
+ls()
+ls(.blotter)
+ls(envir=FinancialInstrument:::.instrument)
+chart_Series(x=SPY,name="SPY",TA="add_SMA(n=50,col=4);add_SMA(n=200,col=2)")
+
+for (i in 1:nrow(SPY)){
+  CurrentDate<-time(SPY)[i]
+  equity=getEndEq(my.strategy,CurrentDate)
+  ClosePrice<-as.numeric(Cl(SPY[i,]))
+  Posn<-getPosQty(my.strategy,Symbol='SPY',Date=CurrentDate)
+  UnitSize=as.numeric(trunc(equity/ClosePrice))
+  FMA<-as.numeric(SPY[i,"MA50"])
+  SMA<-as.numeric(SPY[i,'MA200'])
+  if(!(is.na(FMA) | is.na(SMA))){
+    if(Posn==0){
+      if(FMA>=SMA){
+        addTxn(my.strategy,symbol='SPY',TxnDate = CurrentDate,TxnPrice = ClosePrice,TxnQty = UnitSize, TxnFees = 0)
+      }
+    }else{
+      if(FMA<SMA){
+        addTxn(my.strategy,Symbol = 'SPY',TxnDate = CurrentDate, TxnPrice = ClosePrice,TxnQty=-Posn,TxnFees = 0)
+      } else {
+        if(i==nrow(SPY))
+addTxn(my.strategy,Symbol = 'SPY',TxnDate = CurrentDate,TxnPrice = ClosePrice,TxnQty = -Posn, TxnFees = 0)
+      }
+    }
+  }
+  updatePortf(my.strategy,Dates=CurrentDate)
+  updateAcct(my.strategy,Dates=CurrentDate)
+  updateEndEq(my.strategy,CurrentDate)
+}
